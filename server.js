800c883754ce853d93de88a07662af81e3ca03ed
OwlHD <m.a.flores02@gmail.com>
2023-08-14 18:08:27 -0700

    Updated

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-const express = require('express')
+const express = require('express');
+const pokemonData = require('./database.json')
+const PORT = 5000;
+const app = express();
+app.get('/', (req,res) =>{
+    console.log(pokemonData)
+    res.send("Hello")
+});
+
+app.get('/all-pokemons', (req,res) =>{
+    res.send(JSON.stringify(pokemonData))
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT} successfully!`)
+});
